af62aee7e5de11944a14a3e3f77309ddeaf44eaf
Lari <88565631+romugaboo@users.noreply.github.com>
2024-03-19 23:09:11 -0300

    Questão 5 pronta

--- R/q1_e.R
+++ R/q1_e.R
@@ -1,27 +1,14 @@
-# Carregar pacotes
+# Pacotes e bibliotecas
 source("https://raw.githubusercontent.com/cefet-rj-dal/daltoolbox-examples/main/jupyter.R")
-source("https://raw.githubusercontent.com/eogasawara/mylibrary/master/tutorial/graphics_extra.R")
-
 load_library("daltoolbox")
 load_library("ggplot2")
-load_library("dplyr")
-load_library("reshape")
-load_library("RColorBrewer")
-load_library("corrplot")
-load_library("WVPlots")
-load_library("GGally") 
 
 # Criar dataframe
 wine <- read.table("http://archive.ics.uci.edu/ml/machine-learning-databases/wine/wine.data", header = TRUE, sep = ",") 
 colnames(wine) <- c('class', 'Alcohol', 'Malic', 'Ash', 'Alcalinity', 'Magnesium', 'Phenols', 'Flavanoids', 'Nonflavanoids', 'Proanthocyanins', 'Color', 'Hue', 'Dilution', 'Proline') 
-
 wine$class = as.character(wine$class)
-# Visualizar os ultimos registros do dataframe
-tail(wine)
 
-library(ggplot2)
-#1e
-# Plot scatterplot for each pair of attributes
+# Gráfico de Dispersão
 ggplot(wine_long, aes(x = value, y = as.numeric(variable), color = class)) +
   geom_point() +
   facet_wrap(~ variable, scales = "free") +
--- R/q5.R
+++ R/q5.R
@@ -3,20 +3,11 @@ source("https://raw.githubusercontent.com/cefet-rj-dal/daltoolbox-examples/main/
 load_library("daltoolbox")
 load_library("corrplot")
 
-
 # Criando dataframe
 wine <- read.table("http://archive.ics.uci.edu/ml/machine-learning-databases/wine/wine.data", header = TRUE, sep = ",") 
 colnames(wine) <- c('Type', 'Alcohol', 'Malic', 'Ash', 'Alcalinity', 'Magnesium', 'Phenols', 'Flavanoids', 'Nonflavanoids', 'Proanthocyanins', 'Color', 'Hue', 'Dilution', 'Proline')
 
-# K-means e entropia
-kmeans_model <- kmeans(wine[, -1], centers = 3)
-
-entropy <- function(labels) {
-  probabilities <- table(labels) / length(labels)
-  -sum(probabilities * log2(probabilities))
-}
-group_entropy <- sapply(unique(kmeans_model$cluster), function(group) {
-  cluster_labels <- wine$Type[kmeans_model$cluster == group]
-  entropy(cluster_labels)
-})
-group_entropy
+# Matriz de correlação
+wine$Type <- as.factor(wine$Type) 
+cor_matrix <- cor(wine[, -1])
+corrplot(cor_matrix, method = "color")
