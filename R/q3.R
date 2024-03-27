77f6ff0f1e2e974ba9380dc253d1e1e5f983af18
Larissa Coelho ✨ <88565631+romugaboo@users.noreply.github.com>
2024-03-27 17:43:36 -0300

    Update q3.R

--- R/q3.R
+++ R/q3.R
@@ -1,9 +1,3 @@
-# Pacotes e bibliotecas
-source("https://raw.githubusercontent.com/cefet-rj-dal/daltoolbox-examples/main/jupyter.R")
-load_library("daltoolbox")
-load_library("corrplot")
-
-
 # Criando dataframe
 wine <- read.table("http://archive.ics.uci.edu/ml/machine-learning-databases/wine/wine.data", header = TRUE, sep = ",") 
 colnames(wine) <- c('Type', 'Alcohol', 'Malic', 'Ash', 'Alcalinity', 'Magnesium', 'Phenols', 'Flavanoids', 'Nonflavanoids', 'Proanthocyanins', 'Color', 'Hue', 'Dilution', 'Proline')
@@ -20,3 +14,8 @@ group_entropy <- sapply(unique(kmeans_model$cluster), function(group) {
   entropy(cluster_labels)
 })
 group_entropy
+
+# Calculando a entropia para a classe "Type" original
+entropy_type <- calculate_entropy(wines$Type)
+
+entropy_type
