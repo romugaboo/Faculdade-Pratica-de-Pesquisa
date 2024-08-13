0388b93e0e27b40d06bcc7350786ebbbe95cfd56
Gallicchio Tavares <um4p35504hum4n4@gmail.com>
2024-08-13 17:51:16 -0300

    adiciona filtro

--- createAirlineXnFlights.R
+++ createAirlineXnFlights.R
@@ -1,13 +1,12 @@
-# Carregar o arquivo .RData
-load("bfd/combined_data.rdata")
-
-# Assumindo que o dataframe principal está em 'combined_df'
-df <- combined_df  # Substitua pelo nome correto, se for diferente
-
-# Calcular o número de voos por 'company' e 'type'
 library(dplyr)
 
-result <- df %>%
+df_filtered <- combined_df_2 %>%
+  filter(!is.na(type), type != "X") %>%  # Remove rows with NA or "X" in 'type'
+  group_by(company) %>%
+  filter(n() >= 15000)
+
+# Calculate the number of flights by 'company' and 'type'
+result <- df_filtered %>%
   group_by(company, type) %>%
   summarise(n = n()) %>%
   ungroup() %>%
@@ -16,9 +15,9 @@ result <- df %>%
   ungroup() %>%
   mutate(flightType = case_when(
     type %in% c("N", "R", "E", "H") ~ "D",
-    type == "I" ~ "I",
-    type %in% c("C", "G", "L") ~ "C"
+    type %in% c("I","N/I") ~ "I",
+    type %in% c("C", "G", "L") ~ "C",
   ))
 
-# Salvar o resultado em um arquivo .csv
-write.csv(result, file = "bfd/airlineXnFlights.csv", row.names = FALSE)
+# Export the results to a CSV file
+write.csv(result, file = "airlineXnFlights.csv", row.names = FALSE)
